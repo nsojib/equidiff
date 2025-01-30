e0deca758f4eb63f92b23542d53362c82ed8371a
nsojib <noushad.sust@gmail.com>
2025-01-30 13:48:50 -0500

    eval task

--- run_eval3.sh
+++ run_eval3.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# Define the base output directory and checkpoint path
-BASE_OUTPUT_DIR="data/mug_eval_output"
-CHECKPOINT_PATH="/home/ubuntu/equidiff/data/outputs/2025.01.29/11.30.08_diff_c_mug_cleanup_d1/checkpoints/epoch=0105-test_mean_score=0.820.ckpt"
+
+TASK="coffee"
+CHECKPOINT_PATH="/home/ubuntu/equidiff/data/outputs/2025.01.29/09.58.46_diff_c_coffee_d2/checkpoints/epoch=0180-test_mean_score=0.680.ckpt"
 
 
+BASE_OUTPUT_DIR="data/{$TASK}_eval_output"
 SEEDS=(42 100 1000)
 
 # Loop through each seed and run the command
