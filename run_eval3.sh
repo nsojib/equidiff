76aae268f20271d4f43646bf0c77dbcf09aeca94
nsojib <noushad.sust@gmail.com>
2025-01-30 13:55:17 -0500

    task{

--- run_eval3.sh
+++ run_eval3.sh
@@ -5,7 +5,7 @@ TASK="coffee"
 CHECKPOINT_PATH="/home/ubuntu/equidiff/data/outputs/2025.01.29/09.58.46_diff_c_coffee_d2/checkpoints/epoch=0180-test_mean_score=0.680.ckpt"
 
 
-BASE_OUTPUT_DIR="data/{$TASK}_eval_output"
+BASE_OUTPUT_DIR="data/$TASK_eval_output"
 SEEDS=(42 100 1000)
 
 # Loop through each seed and run the command
